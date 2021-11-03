4b54da6430a54d69038e0183b7a94d24e0912a78
gilsun <gilsun0821@gmail.com>
2021-11-03 16:28:19 -0700

    add button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 function App() {
+  let [title, setTitle] = useState([
+    "남자코트 추천",
+    "강남우동맛집",
+    "파이썬독학",
+  ]);
+  let [따봉, 따봉변경] = useState(0);
+  const addup = () => {
+    따봉변경(따봉 + 1);
+  };
+  const 제목변경 = () => {
+    setTitle(["여자코트 추천", "강남우동맛집", "파이썬독학"]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <button onClick={제목변경}>버튼</button>
+      <div className="list">
+        <h4>
+          {title[0]}
+          <span onClick={addup}> 👍</span> {따봉}
+        </h4>
+        <p>2월 17일 발행 </p>
+        <hr />
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행 </p>
+        <hr />
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>2월 17일 발행 </p>
+        <hr />
+      </div>
     </div>
   );
 }
-
 export default App;
