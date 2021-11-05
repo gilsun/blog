08fa0cc783162ca8afe62db0ff335c819538c9d3
gilsun <gilsun0821@gmail.com>
2021-11-05 00:00:28 -0700

    add up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 function App() {
-  let [title, setTitle] = useState([
+  let [글제목, 글제목변경] = useState([
     "남자코트 추천",
     "강남우동맛집",
     "파이썬독학",
@@ -11,7 +11,11 @@ function App() {
     따봉변경(따봉 + 1);
   };
   const 제목변경 = () => {
-    setTitle(["여자코트 추천", "강남우동맛집", "파이썬독학"]);
+    let newArray = [...글제목];
+    newArray[0] = "여자코트 추천";
+    newArray[1] = "강남맛집 추천";
+    newArray[2] = "자바스크립트 추천";
+    글제목변경(newArray);
   };
   return (
     <div className="App">
@@ -21,19 +25,19 @@ function App() {
       <button onClick={제목변경}>버튼</button>
       <div className="list">
         <h4>
-          {title[0]}
+          {글제목[0]}
           <span onClick={addup}> 👍</span> {따봉}
         </h4>
         <p>2월 17일 발행 </p>
         <hr />
       </div>
       <div className="list">
-        <h4>{title[1]}</h4>
+        <h4>{글제목[1]}</h4>
         <p>2월 17일 발행 </p>
         <hr />
       </div>
       <div className="list">
-        <h4>{title[2]}</h4>
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행 </p>
         <hr />
       </div>
